73ac77c6aae08ca5569d50e0d758573d0c2b9404
Thomas Laure <thomaslaure3@gmail.com>
2025-10-18 12:04:37 +0200

    refactor: replace params by DTO press

--- src/Service/ZendeskService.php
+++ src/Service/ZendeskService.php
@@ -4,6 +4,7 @@ namespace MobilityWork\Service;
 
 use MobilityWork\Domain\Model\Ticket\CreateCustomerTicketRequest;
 use MobilityWork\Domain\Model\Ticket\CreateHotelTicketRequest;
+use MobilityWork\Domain\Model\Ticket\CreatePressTicketRequest;
 use MobilityWork\Repository\ReservationRepository;
 use Zendesk\API\HttpClient as ZendeskAPI;
 
@@ -70,10 +71,9 @@ class ZendeskService extends AbstractService
             [
                 'requester_id' => $response->user->id,
                 'subject' => 50 < strlen($request->message) ? substr($request->message, 0, 50).'...' : $request->message,
-                'comment' =>
-                    [
-                        'body'  => $request->message,
-                    ],
+                'comment' => [
+                    'body'  => $request->message,
+                ],
                 'priority'      => 'normal',
                 'type'          => 'question',
                 'status'        => 'new',
@@ -115,10 +115,9 @@ class ZendeskService extends AbstractService
             [
                 'requester_id' => $response->user->id,
                 'subject' => strlen($request->message) > 50 ? substr($request->message, 0, 50).'...' : $request->message,
-                'comment' =>
-                    [
-                        'body' => $request->message,
-                    ],
+                'comment' => [
+                    'body' => $request->message,
+                ],
                 'priority' => 'normal',
                 'type' => 'question',
                 'status' => 'new',
@@ -129,41 +128,29 @@ class ZendeskService extends AbstractService
         return true;
     }
 
-    public function createPressTicket(
-        $gender,
-        $firstName,
-        $lastName,
-        $country,
-        $phoneNumber,
-        $email,
-        $city,
-        $media,
-        $subject,
-        $message,
-        $language,
-        $domainConfig)
+    public function createPressTicket(CreatePressTicketRequest $request): bool
     {
         $customFields = [];
         $customFields['80924888'] = 'press';
-        $customFields['80918648'] = $city;
-        $customFields['80918708'] = $language->getName();
+        $customFields['80918648'] = $request->city;
+        $customFields['80918708'] = $request->language->getName();
 
         $client = new ZendeskAPI($this->getServiceManager()->get('Config')['zendesk']['subdomain']);
         $client->setAuth(
             'basic',
             [
                 'username' => $this->getServiceManager()->get('Config')['zendesk']['username'],
-                'token' => $this->getServiceManager()->get('Config')['zendesk']['token']
+                'token' => $this->getServiceManager()->get('Config')['zendesk']['token'],
             ]
         );
 
         $response = $client->users()->createOrUpdate(
             [
-                'email' => $email,
-                'name' => $firstName.' '.strtoupper($lastName),
-                'phone' => $phoneNumber,
+                'email' => $request->email,
+                'name' => $request->firstName.' '.strtoupper($request->lastName),
+                'phone' => $request->phoneNumber,
                 'role' => 'end-user',
-                'user_fields' => [ 'press_media' => $media ]
+                'user_fields' => ['press_media' => $request->media],
             ]
         );
 
@@ -171,15 +158,14 @@ class ZendeskService extends AbstractService
             $client->tickets()->create(
                 [
                     'requester_id' => $response->user->id,
-                    'subject' => strlen($message) > 50 ? substr($message, 0, 50) . '...' : $message,
-                    'comment' =>
-                        [
-                            'body' => $message
-                        ],
+                    'subject' => 50 < strlen($request->message) ? substr($request->message, 0, 50).'...' : $request->message,
+                    'comment' => [
+                        'body' => $request->message,
+                    ],
                     'priority' => 'normal',
                     'type' => 'question',
                     'status' => 'new',
-                    'custom_fields' => $customFields
+                    'custom_fields' => $customFields,
                 ]
             );
         } catch (\Exception $e) {
