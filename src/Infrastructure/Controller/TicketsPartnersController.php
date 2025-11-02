353f81ddd2c17f93bf69238c1478237e16f7d0b5
Thomas Laure <thomaslaure3@gmail.com>
2025-11-02 11:18:13 +0100

    feat: create ticket partners route

---
+++ src/Infrastructure/Controller/TicketsPartnersController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MobilityWork\Infrastructure\Controller;
+
+use MobilityWork\Application\UseCase\CreatePartnersTicket\CreatePartnersTicketCommand;
+use MobilityWork\Domain\Model\Ticket\CreatePartnersTicketRequest;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/tickets/partners', name: 'app_tickets_partners', methods: [Request::METHOD_POST])]
+final class TicketsPartnersController extends AbstractController
+{
+    public function __construct(
+        private readonly MessageBusInterface $bus,
+    ) {
+    }
+
+    public function __invoke(
+        #[MapRequestPayload]
+        CreatePartnersTicketRequest $request,
+    ): JsonResponse {
+        $this->bus->dispatch(new CreatePartnersTicketCommand($request));
+
+        return $this->json([
+            'message' => 'Partners ticket creation request received.',
+        ], Response::HTTP_ACCEPTED);
+    }
+}
