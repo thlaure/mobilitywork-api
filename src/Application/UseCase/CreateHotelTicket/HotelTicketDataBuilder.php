e3bbb687c815ef75eb49a9036a0c564712d67a43
Thomas Laure <thomaslaure3@gmail.com>
2025-11-09 12:19:03 +0100

    refactor: change data structure for hotel ticket

--- src/Application/UseCase/CreateHotelTicket/HotelTicketDataBuilder.php
+++ src/Application/UseCase/CreateHotelTicket/HotelTicketDataBuilder.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace MobilityWork\Application\UseCase\CreateHotelTicket;
 
+use MobilityWork\Domain\Model\Data\TicketCreationDataDTO;
+use MobilityWork\Domain\Model\Data\TicketDTO;
+use MobilityWork\Domain\Model\Data\UserDTO;
 use MobilityWork\Domain\Model\Entity\Language;
 use MobilityWork\Domain\Port\Out\LanguageRepositoryPort;
 use MobilityWork\Infrastructure\Zendesk\Constants\ZendeskCustomFields;
@@ -15,14 +18,10 @@ final class HotelTicketDataBuilder
     ) {
     }
 
-    public function build(CreateHotelTicketCommand $command): HotelTicketDataDTO
+    public function build(CreateHotelTicketCommand $command): TicketCreationDataDTO
     {
         /** @var ?Language $language */
-        $language = null;
-
-        if (null !== $command->request->languageId) {
-            $language = $this->languageRepository->findOneById($command->request->languageId);
-        }
+        $language = $this->languageRepository->findOneById($command->request->languageId);
 
         $customFields = [];
         $customFields[ZendeskCustomFields::TICKET_TYPE] = 'hotel';
@@ -33,25 +32,23 @@ final class HotelTicketDataBuilder
             $customFields[ZendeskCustomFields::LANGUAGE_NAME] = $language->getName();
         }
 
-        $user = [
-            'email' => $command->request->email,
-            'name' => $command->request->firstName.' '.strtoupper($command->request->lastName),
-            'phone' => $command->request->phoneNumber,
-            'role' => 'end-user',
-            'user_fields' => ['website' => $command->request->website],
-        ];
+        $user = new UserDTO(
+            email: $command->request->email,
+            name: $command->request->firstName.' '.strtoupper($command->request->lastName),
+            phone: $command->request->phoneNumber,
+            userFields: [
+                'website' => $command->request->website,
+            ]
+        );
 
-        $ticket = [
-            'subject' => strlen($command->request->message) > 50 ? substr($command->request->message, 0, 50).'...' : $command->request->message,
-            'comment' => [
+        $ticket = new TicketDTO(
+            subject: strlen($command->request->message) > 50 ? substr($command->request->message, 0, 50).'...' : $command->request->message,
+            comment: [
                 'body' => $command->request->message,
             ],
-            'priority' => 'normal',
-            'type' => 'question',
-            'status' => 'new',
-            'custom_fields' => $customFields,
-        ];
+            customFields: $customFields
+        );
 
-        return new HotelTicketDataDTO($user, $ticket);
+        return new TicketCreationDataDTO($user, $ticket);
     }
 }
